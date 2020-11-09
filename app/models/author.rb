8c3575479b1206c0c478087007f61dc043e2fb97
PatriciaSowa <patricia.sowa@student.hpi.de>
2020-11-09 20:55:51 +0100

    Add constructor new

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,11 @@
 class Author < ApplicationRecord
+    def new(first_name,last_name, homepage)
+        self.first_name = first_name
+        self.last_name = last_name
+        self.homepage = homepage
+    end
+
     def name
          self.first_name + ' ' + self.last_name
-      end
+    end
 end
