c4bfa5a674561a9d59318e4dba9c39be5cf954b0
PatriciaSowa <patricia.sowa@student.hpi.de>
2020-11-09 20:50:40 +0100

    Fix test 4?

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -4,7 +4,7 @@ describe "Create Author-Instance", type: :model do
 
   it "The author model should have a first_name, last_name and homepage, which are all strings.
       It should furthermore have a method name which returns the full name of an author (the first name followed by the last name), e.g. 'Alan Turing'." do
-    author = Author.new(first_name="Alan", last_name="Turing", homepage="http://wikipedia.org/Alan_Turing")
+    author = Author.new("Alan", "Turing", "http://wikipedia.org/Alan_Turing")
     expect(author.first_name).to eq("Alan")
     expect(author.last_name).to eq("Turing")
     expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
