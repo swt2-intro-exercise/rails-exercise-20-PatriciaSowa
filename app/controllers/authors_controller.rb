c5e4c7bbaddaa8c3845c59453b639e2def33d4f2
PatriciaSowa <patricia.sowa@student.hpi.de>
2020-11-09 20:42:05 +0100

    Tries to fiy 4th test

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,8 +1,22 @@
 class AuthorsController < ApplicationController
+    def show
+        @author = Author.find(params[:id])
+      end
+
     def new
       end
 
     def create
-      render plain: params[:author].inspect
+      # render plain: params[:author].inspect
+      # end
+      @author = Author.new(author_params)
+
+      @author.save
+      redirect_to @author
       end
+
+      private
+        def author_params
+          params.require(:author).permit(:first_name, :last_name, :homepage)
+        end
 end
