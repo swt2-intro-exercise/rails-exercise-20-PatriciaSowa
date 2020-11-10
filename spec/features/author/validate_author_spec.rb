2d114f332d87d9b21c838199a8b1f2d50f70e950
PatriciaSowa <patricia.sowa@student.hpi.de>
2020-11-10 16:34:04 +0100

    Fix issue 6

--- spec/features/author/validate_author_spec.rb
+++ spec/features/author/validate_author_spec.rb
@@ -5,7 +5,7 @@ describe "Show page of an author", type: :feature do
   it "should show first_name, last_name and homepage of the author" do
     author = Author.new(first_name: "Alan", last_name: "", homepage: "http://wikipedia.org/Alan_Turing")
 
-    expect(@author).to_not be_valid
+    expect(author).to_not be_valid
   end
 
 end
