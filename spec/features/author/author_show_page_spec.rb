8c9655a370c959614fb6c4753011c943e9ae2826
PatriciaSowa <patricia.sowa@student.hpi.de>
2020-11-10 15:31:55 +0100

    Fix issue 4

--- spec/features/author/author_show_page_spec.rb
+++ spec/features/author/author_show_page_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 describe "Show page of an author", type: :feature do
 
   it "should show first_name, last_name and homepage of the author" do
-    FactoryBot.create :author
-    expect(page).to have_text("Alan", "Turing", "http://wikipedia.org/Alan_Turing")
+    @alan = FactoryBot.create :author
+    visit author_path(@alan)
+    expect(page).to have_text("Alan")
+    expect(page).to have_text("Turing")
+    expect(page).to have_text("http://wikipedia.de/Alan_Turing")
   end
+
+end
