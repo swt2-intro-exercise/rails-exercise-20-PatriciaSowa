41ce80062ae2ef9acdd6ff87721a431f7a20587e
PatriciaSowa <patricia.sowa@student.hpi.de>
2020-11-10 19:49:47 +0100

    Add test for issue 9

---
+++ spec/features/author/delete_author_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "Delete Author", type: :feature do
+
+  it "When users visit the authors index page, then it should have link to delete an author" do
+        @alan = FactoryBot.create :author
+        visit authors_path
+        expect(page).to have_link 'Delete', href: author_path(@alan)
+
+        count_before = Author.count
+        @alan.destroy
+        count_after = Author.count
+
+        expect(count_before-count_after).to be > 0
+  end
+
+end
