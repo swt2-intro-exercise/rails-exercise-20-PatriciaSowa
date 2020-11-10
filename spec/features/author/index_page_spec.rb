f9d477fba9daabd57974e583ae4f5f20d86147b3
PatriciaSowa <patricia.sowa@student.hpi.de>
2020-11-10 16:23:18 +0100

    Fix issue 5

--- spec/features/author/index_page_spec.rb
+++ spec/features/author/index_page_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 describe "Author index page", type: :feature do
 
   it "when a user visits the author index page, then the page should render" do
-
-    visit root_path
+    @alan = FactoryBot.create :author
+    visit authors_path
+    expect(page).to have_link 'Show', href: author_path(@alan)
     expect(page).to have_link 'New', href: new_author_path
   end
 
