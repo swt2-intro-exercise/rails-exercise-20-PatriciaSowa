b2be85c81406cdaf3270bc75fd20585148d3f03f
PatriciaSowa <patricia.sowa@student.hpi.de>
2020-11-11 20:04:33 +0100

    Add test for issue 15

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,5 +1,7 @@
 class Paper < ApplicationRecord
-    has_and_belongs_to_many :authors
+    has_and_belongs_to_many :
+
+     scope :year, -> { where(year: params[:year]) }
 
     validates :title, presence: true, length: { minimum: 1 }
     validates :venue, presence: true, length: { minimum: 1 }
---
+++ spec/features/paper/filter_paper_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "Filter papers by year", type: :feature do
+
+    it "When users visit the papers index page with url parameter year=1950, then it should not show the paper published in 1968" do
+        paper1 = Paper.new(title:"Some title", venue:"Some venue", year:2020)
+        paper2 = Paper.new(title:"Other title", venue:"Other venue", year:2000)
+        paper1.save
+        paper2.save
+        visit index_path("2000")
+        expect(page).to_not have_text("2020")
+    end
+end
