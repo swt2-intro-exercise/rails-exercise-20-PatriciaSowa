ce83ecc4356afedec86f5560e17b5ce0d9993976
PatriciaSowa <patricia.sowa@student.hpi.de>
2020-11-12 08:48:42 +0100

    Add test for issue 16

---
+++ spec/features/paper/paper_index_page_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe "Author index page", type: :feature do
+
+  it "when a user visits the author index page, then the page should render" do
+    @computing = FactoryBot.create :paper
+    visit papers_path
+    expect(page).to have_text("COMPUTING MACHINERY AND INTELLIGENCE")
+  end
+
+end
