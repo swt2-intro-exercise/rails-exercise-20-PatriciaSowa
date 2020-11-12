e00fa8d6a95893cc753f0d4cb8323c4454aa1aca
PatriciaSowa <patricia.sowa@student.hpi.de>
2020-11-12 09:25:10 +0100

    Fix issue 16

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,7 +1,7 @@
 class Paper < ApplicationRecord
     has_and_belongs_to_many :authors
 
-    scope :year, ->(number) { where("year = ?", number) }
+    scope :year, ->(number) { where("year = ?", number) if number.present? }
 
     validates :title, presence: true, length: { minimum: 1 }
     validates :venue, presence: true, length: { minimum: 1 }
--- spec/features/paper/filter_paper_spec.rb
+++ spec/features/paper/filter_paper_spec.rb
@@ -7,7 +7,7 @@ describe "Filter papers by year", type: :feature do
         paper2 = Paper.new(title:"Other title", venue:"Other venue", year:2000)
         paper1.save
         paper2.save
-        visit papers_path
+        visit papers_path(year: 2000)
         expect(page).not_to have_text("2020")
     end
 end
